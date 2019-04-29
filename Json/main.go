04e506f2d2ef3cc0fb54363b949d9f1ae650d8a8
Sachith Muhandiram <sachith@vizuamatix.com>
2019-04-29 21:47:12 +0530

    Unmarshal example added

--- Json/main.go
+++ Json/main.go
@@ -4,12 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 )
+/*
 
+here I have updated struct with json tags, which are used to do
+Unmarshal. Tags are necessory for mapping byte of data into data
+
+*/
 type person struct {
-	Fname string
-	Lname string
-	Age   int
+	Fname string `json:"Fname"`
+	Lname string `json:"Lname"`
+	Age   int `json:"Age"`
 }
+
 
 func main() {
 	p1 := person{
@@ -30,6 +36,7 @@ func main() {
 	people := []person{p1, p2} // slice of persons
 
 	fmt.Println(people)
+	// [{Sachith Muhandiram 29} {Linus Linux 45}]
 
 	ppl, err := json.Marshal(people)
 
@@ -38,4 +45,17 @@ func main() {
 	}
 
 	fmt.Println(string(ppl)) // remember to convert byte stream to string
+	fmt.Println(ppl)
+	/*
+		The result byte stream
+		[91 123 34 70 110 97 109 101 34 58 34 83 97 99 104 105 116 104 34 44 34 76 110 97 109 101 34 58 34 77 117 104 97 110 100 105 114 97 109 34 44 34 65 103 101 34 58 50 57 125 44 123 34 70 110 97 109 101 34 58 34 76 105 110 117 115 34 44 34 76 110 97 109 101 34 58 34 76 105 110 117 120 34 44 34 65 103 101 34 58 52 53 125 93]
+	*/
+
+	err = json.Unmarshal(ppl, &people) // byte stream and mem location of struct values
+	
+	if err != nil{
+		fmt.Println("Error : ",err)
+	}
+
+	fmt.Println(people)
 }
